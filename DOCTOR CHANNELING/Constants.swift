09ed88f3c09e755a6680ad636fd1c42a56bb77bd
Ivan Biriukov <Grottt0@mail.ru>
2023-06-19 14:24:42 +0300

    Upload UI Design Assets; Create Constants file;

---
+++ DOCTOR CHANNELING/Constants.swift
@@ -0,0 +1,55 @@
+
+import Foundation
+
+struct K {
+    
+    static let appIcon : String = "appIcon"
+    
+    struct Avatars {
+        static let userAvatarIcon : String = "avatarIcon"
+        static let firstDoctorAvatar : String = "DoctorAvatar_1"
+        static let secondDoctorAvatar : String = "DoctorAvatar_2"
+        static let thirdDoctorAvatar: String = "DoctorAvatar_3"
+        static let fourthDoctorAvatar : String = "DoctorAvatar_4"
+        static let fifthDoctorAvatar : String = "DoctorAvatar_5"
+        static let sixthDoctorAvatar : String = "DoctorAvatar_6"
+    }
+    
+    struct Doctors {
+        static let firstDoctorImage : String = "Donctor_1"
+        static let secondDoctorImage : String = "Doctor_2"
+        static let thirdDoctorImage : String = "Doctor_3"
+        static let fourthDoctorImage : String = "Doctor_4"
+    }
+    
+    struct ButtonsImgs {
+        static let facebook : String = "FacebookButtonIcon"
+        static let google : String = "GoogleButtonIcon"
+        static let mic : String = "MicButton"
+        static let send :  String = "SendButton"
+        
+        struct NavBarButtons {
+            static let back : String = "BarBackButton"
+            static let call : String = "BarCallButton"
+            static let video : String = "BarVideoButton"
+            static let ring : String = "RingButton"
+        }
+        
+        struct TabBarButtons {
+            static let home : String = "HomeButton"
+            static let clock : String = "ClockButton"
+            static let chat : String = "ChatButton"
+            static let profile : String = "ProfileButton"
+        }
+        
+        struct ProfileButtons {
+            static let history : String = "HistoryButton"
+            static let personalData : String = "PersonalInfoButton"
+            static let location : String = "LocationButton"
+            static let payment : String = "PaymentButton"
+            static let setting : String = "SettingsButton"
+            static let help : String = "HelpButton"
+            static let logOut : String = "LogoutButton"
+        }
+    }
+}
