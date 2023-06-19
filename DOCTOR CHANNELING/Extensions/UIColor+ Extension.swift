159ddc5518e3069e7caee38fdcb20292e68b12b0
Ivan Biriukov <Grottt0@mail.ru>
2023-06-19 03:52:21 +0300

    Initial commit; Delete storyboard; Add fonts + colors + extensions for it; Upload some placeholder assets

---
+++ DOCTOR CHANNELING/Extensions/UIColor+ Extension.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+extension UIColor {
+
+    // MARK: - Background Colors
+    
+    static let lightBackgroundColor = UIColor(red: 197/256, green: 227/256, blue: 237/256, alpha: 1)
+    
+    static let lightGrayBackgroundColor = UIColor(red: 217/256, green: 217/256, blue: 217/256, alpha: 1)
+    
+    static let middleGrayBackgroundColor = UIColor(red: 133/256, green: 133/256, blue: 133/256, alpha: 1)
+    
+    // MARK: - Text Colors
+    
+    static let textLightBlue = UIColor(red: 25/256, green: 142/256, blue: 182/256, alpha: 1)
+    
+    static let textLightGray = UIColor(red: 133/256, green: 133/256, blue: 133/256, alpha: 1)
+    
+    static let textUnavalibleGray = UIColor(red: 144/256, green: 144/256, blue: 144/256, alpha: 1)
+    
+    // MARK: - UIElements Colors
+    
+    static let uiGold = UIColor(red: 248/256, green: 150/256, blue: 3/256, alpha: 1)
+    
+    static let uiGreen = UIColor(red: 21/256, green: 184/256, blue: 38/256, alpha: 1)
+    
+    static let uiRed = UIColor(red: 243/256, green: 0/256, blue: 0/256, alpha: 1)
+}
---
+++ DOCTOR CHANNELING/Extensions/UIFont+ Extension.swift
@@ -0,0 +1,82 @@
+import UIKit
+
+extension UIFont {
+    
+    // MARK: - AveriaSansLibre-Bold
+    
+    static func averiaBold50() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Bold", size: 50)
+    }
+    
+    static func averiaBold40() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Bold", size: 40)
+    }
+    
+    static func averiaBold30() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Bold", size: 30)
+    }
+    
+    static func averiaBold26() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Bold", size: 26)
+    }
+    
+    static func averiaBold24() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Bold", size: 24)
+    }
+    
+    static func averiaBold22() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Bold", size: 22)
+    }
+    
+    static func averiaBold20() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Bold", size: 20)
+    }
+    
+    // MARK: - AveriaSansLibre-Regular
+    
+    static func averiaRegular12() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 12)
+    }
+    
+    static func averiaRegular13() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 13)
+    }
+    
+    static func averiaRegular15() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 15)
+    }
+    
+    static func averiaRegular16() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 16)
+    }
+    
+    static func averiaRegular18() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 18)
+    }
+    
+    static func averiaRegular20() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 20)
+    }
+    
+    static func averiaRegular22() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 22)
+    }
+    
+    static func averiaRegular24() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 22)
+    }
+    
+    static func averiaRegular26() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 26)
+    }
+    
+    static func averiaRegular28() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 28)
+    }
+    
+    // MARK: - MontserratAlternates-SemiBold
+    
+    static func montserratSemiBold16() -> UIFont?{
+        return UIFont(name: "AveriaSansLibre-Regular", size: 16)
+    }
+}
