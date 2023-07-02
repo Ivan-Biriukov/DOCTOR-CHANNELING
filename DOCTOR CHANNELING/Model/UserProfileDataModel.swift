d2d9794b3f3f67d730c1d228c4c9b83e982a7ba6
Ivan Biriukov <Grottt0@mail.ru>
2023-07-03 02:28:43 +0300

    Add FireStore init

--- DOCTOR CHANNELING/Model/UserProfileDataModel.swift
+++ DOCTOR CHANNELING/Model/UserProfileDataModel.swift
@@ -1,6 +1,11 @@
 import Foundation
 
 struct UserProfileDataModel {
+    let userName : String?
+    let userEmail: String
+    let userPassword: String
+    let userPhoneNumber: String?
+    let userAvatar : String?
     let cardInfo : UserCardInfo?
 }
 
--- DOCTOR CHANNELING/Supporting files/AppDelegate.swift
+++ DOCTOR CHANNELING/Supporting files/AppDelegate.swift
@@ -1,5 +1,6 @@
 import IQKeyboardManagerSwift
 import FirebaseCore
+import FirebaseFirestore
 import UIKit
 import GoogleSignIn
 
@@ -9,6 +10,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         IQKeyboardManager.shared.enable = true
         FirebaseApp.configure()
+        let db = Firestore.firestore()
         return true
     }
     
